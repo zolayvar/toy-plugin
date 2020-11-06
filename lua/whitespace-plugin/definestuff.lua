809999e22b7fb7338969dbcf64721f14d65c428a
Stephanie Zolayvar <zolayvars@gmail.com>
2020-11-05 18:06:05 -0800

    example skeleton

---
+++ lua/whitespace-plugin/definestuff.lua
@@ -0,0 +1,16 @@
+local function show_stuff()
+  print "whitespace-plugin.definestuff show_stuff: hello"
+end
+
+-- Returning a Lua table at the end allows fine control of the symbols that
+-- will be available outside this file. By returning the table, it allows the
+-- importer to decide what name to use in their own code.
+--
+-- Examples:
+--    local ds = require('myluamodule/definestuff')
+--    ds.show_stuff()
+--    local definestuff = require('myluamodule/definestuff')
+--    definestuff.show_stuff()
+return {
+  show_stuff = show_stuff,
+}
