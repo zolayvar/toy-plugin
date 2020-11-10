f43f83c97e7c6c1ac66a64f1e531e01ef23df668
Stephanie Zolayvar <zolayvars@gmail.com>
2020-11-10 15:10:37 -0800

    g

---
+++ lua/toy-plugin/lua.lua
@@ -0,0 +1,8 @@
+
+local function delete_trailing_whitespace()
+  print "hello i am the lua function you have called me"
+end
+
+return {
+  delete_trailing_whitespace = delete_trailing_whitespace,
+}
