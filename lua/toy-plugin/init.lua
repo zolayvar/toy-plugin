dd05ccd5eccd0947a0bb3b4b1d165ff55ce28d4f
Stephanie Zolayvar <zolayvars@gmail.com>
2020-11-10 23:02:17 -0800

    f

--- lua/toy-plugin/init.lua
+++ lua/toy-plugin/init.lua
@@ -1,7 +1,6 @@
 local tp = {}
 
 tp.delete_trailing_whitespace = function()
-  print "hello i am the lua function i will try to delete the trailing whitespace"
   vim.api.nvim_command("%s/\\s\\+$//g")
 end
 
--- plugin/toy-plugin.vim
+++ plugin/toy-plugin.vim
@@ -1,2 +1,2 @@
 lua tp = require("toy-plugin")
-nmap <M-C-L> :lua tp.delete_trailing_whitespace()<CR>
+nmap <M-C-W> :lua tp.delete_trailing_whitespace()<CR>
