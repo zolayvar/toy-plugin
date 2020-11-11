ed8b181d2efb0333adc0664a66a6636f0e7e9aa7
Stephanie Zolayvar <zolayvars@gmail.com>
2020-11-10 18:47:07 -0800

    v

--- lua/toy-plugin/init.lua
+++ lua/toy-plugin/init.lua
@@ -1,8 +1,8 @@
 local tp = {}
 
 tp.delete_trailing_whitespace = function()
-  print "hello i am the lua function you have called me"
-  vim.api.nvim_command('%s/\s+$//g')
+  print "hello i am the lua function you have called me once more"
+  vim.api.nvim_command("%s/\s+$//g")
 end
 
 return tp
