aec57feb01b7ad338d6d79e72f980512c8753260
Stephanie Zolayvar <zolayvars@gmail.com>
2020-11-11 19:06:48 -0800

    f

--- lua/toy-plugin/init.lua
+++ lua/toy-plugin/init.lua
@@ -1,12 +1,11 @@
 local tp = {}
 
 tp.delete_trailing_whitespace = function()
-  vim.api.nvim_command("%s/\\s\\+$//g")
+  vim.api.nvim_command([[%s/\s\+$//g]])
 end
 
 tp.inner_indent_comment = function()
-  vim.api.nvim_command(":'<,'> s/\\/\\//\\/\\/  /")
-  vim.api.nvim_input("gv")
+  vim.api.nvim_command([[:'<,'> s/\/\//\/\/  / | gv]])
 end
 
 return tp
